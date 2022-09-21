aebb2cf0ea4524dc822f7f1ab0660226460a9270
Ivan Petrov <lazytechwork@gmail.com>
2022-09-21 22:13:24 +0300

    Initial commit

---
+++ src/test/java/me/lazytechwork/algods/TrueTest.java
@@ -0,0 +1,11 @@
+package me.lazytechwork.algods;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TrueTest {
+    @Test
+    void isTrueReallyTrue() {
+        Assertions.assertTrue(true, "True is not true");
+    }
+}
