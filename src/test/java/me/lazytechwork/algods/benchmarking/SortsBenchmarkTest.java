df25eecb48cc51af5063260365912235af9e5025
Ivan Petrov <lazytechwork@gmail.com>
2022-10-26 01:52:58 +0300

    Фикс форматирования и обкатка на сортировке пузырьком

--- src/test/java/me/lazytechwork/algods/benchmarking/SortsBenchmarkTest.java
+++ src/test/java/me/lazytechwork/algods/benchmarking/SortsBenchmarkTest.java
@@ -7,16 +7,19 @@ import org.junit.jupiter.api.Test;
 import org.knowm.xchart.VectorGraphicsEncoder;
 import org.knowm.xchart.XYChart;
 import org.knowm.xchart.XYSeries;
+import org.knowm.xchart.style.Styler;
 import org.knowm.xchart.style.markers.SeriesMarkers;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Random;
 
 public class SortsBenchmarkTest {
-    public static Benchmark benchmark;
-    private static final List<Integer> counts = List.of(1000, 10000, 100000, 1000000, 10000000);
+    private static Benchmark benchmark;
+    private static final List<Integer> counts = List.of(10, 15, 25, 50, 100, 125, 150, 200, 500, 750, 1500, 2500, 4350, 5000, 7565, 10000);
+    private static final Random random = new Random();
 
     @BeforeAll
     static void beforeAll() {
@@ -57,15 +60,27 @@ public class SortsBenchmarkTest {
     void benchmarkBubbleSort() {
         counts.forEach((count) -> {
             String benchmarkName = "BubbleSort %d".formatted(count);
+            int[] arr = new int[count];
+            for (int i = 0; i < count; i++)
+                arr[i] = random.nextInt();
+
             benchmark.startBenchmark(benchmarkName);
-            // TODO sort
+            int n = count;
+            for (int i = 0; i < n - 1; i++)
+                for (int j = 0; j < n - i - 1; j++)
+                    if (arr[j] > arr[j + 1]) {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
             benchmark.stopBenchmark(benchmarkName);
         });
     }
 
     @AfterAll
     static void afterAll() throws IOException {
-        XYChart chart = new XYChart(500, 400);
+        // @formatter:off
+        XYChart chart = new XYChart(500, 400, Styler.ChartTheme.Matlab);
         chart.setTitle("Sorts benchmarking");
         chart.setXAxisTitle("Elements sorted");
         chart.setYAxisTitle("Time");
@@ -91,5 +106,7 @@ public class SortsBenchmarkTest {
         });
 
         VectorGraphicsEncoder.saveVectorGraphic(chart, "./build/reports/SortsBenchmark.svg", VectorGraphicsEncoder.VectorGraphicsFormat.SVG);
+
+        // @formatter:on
     }
 }
